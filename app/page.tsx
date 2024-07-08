b6fcefd5b48e094c59d1fa1ea0142a85b318241b
piyush wankhade <piyush.wankhade@fulcrumdigital.com>
2024-07-08 22:33:37 +0530

    Use new testimonial component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import { TestimonialCarousel } from "@piyushwankhade/tii-components";
+import { TestimonialCarousel,TestimonialList } from "@piyushwankhade/tii-components";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TestimonialCarousel/>
+      <TestimonialList/>
     </main>
   );
 }
